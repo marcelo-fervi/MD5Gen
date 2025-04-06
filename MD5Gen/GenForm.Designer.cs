a0fc27761e1b062ddc4d76fec07c0ad64eade07f
marcelo-fervi <35692178+marcelo-fervi@users.noreply.github.com>
2025-04-06 20:21:20 -0300

    Initial commit

---
+++ MD5Gen/GenForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace gen5
+{
+    partial class GenForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.normalTextBox = new System.Windows.Forms.TextBox();
+            this.md5ResultTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // normalTextBox
+            // 
+            this.normalTextBox.Location = new System.Drawing.Point(41, 8);
+            this.normalTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.normalTextBox.MaxLength = 33;
+            this.normalTextBox.Multiline = true;
+            this.normalTextBox.Name = "normalTextBox";
+            this.normalTextBox.Size = new System.Drawing.Size(366, 25);
+            this.normalTextBox.TabIndex = 1;
+            this.normalTextBox.TextChanged += new System.EventHandler(this.normalTextBox_TextChanged);
+            // 
+            // md5ResultTextBox
+            // 
+            this.md5ResultTextBox.Location = new System.Drawing.Point(41, 35);
+            this.md5ResultTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.md5ResultTextBox.Multiline = true;
+            this.md5ResultTextBox.Name = "md5ResultTextBox";
+            this.md5ResultTextBox.ReadOnly = true;
+            this.md5ResultTextBox.Size = new System.Drawing.Size(366, 25);
+            this.md5ResultTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Text:";
+            // 
+            // GenForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 67);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.md5ResultTextBox);
+            this.Controls.Add(this.normalTextBox);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "GenForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MD5Gen";
+            this.Load += new System.EventHandler(this.GenForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox normalTextBox;
+        private System.Windows.Forms.TextBox md5ResultTextBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MD5Gen/GenForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace gen5
+{
+    public partial class GenForm : Form
+    {
+        private const string md5Key = "";
+        public GenForm()
+        {
+            InitializeComponent();
+        }
+        private void normalTextBox_TextChanged(object sender, EventArgs e)
+        {
+            md5ResultTextBox.Text = GenerateTextMD5(normalTextBox.Text);
+        }
+        private static string GenerateTextMD5(string text)
+        {
+            using (var hmacMD5 = new HMACMD5(Encoding.UTF8.GetBytes(md5Key)))
+            {
+                byte[] data = hmacMD5.ComputeHash(Encoding.UTF8.GetBytes(text));
+
+                StringBuilder sBuilder = new StringBuilder();
+                for (int i = 0; i < data.Length; i++)
+                    sBuilder.Append(data[i].ToString("x2"));
+
+                return sBuilder.ToString();
+            }
+        }
+
+        private void GenForm_Load(object sender, EventArgs e)
+        {
+            if (md5Key == "")
+            {
+                MessageBox.Show("There's no key defined to generate hashes!\nDon't forget to define one.", null, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+    }
+}
